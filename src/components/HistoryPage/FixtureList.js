b7082ca7f10a4dc3ac4b09527eff4159169c7686
kubasikora <J.Sikora@stud.elka.pw.edu.pl>
2018-09-15 22:09:51 +0200

    properly expandable history page

--- src/components/HistoryPage/FixtureList.js
+++ src/components/HistoryPage/FixtureList.js
@@ -7,69 +7,73 @@ import CircularProgress from "@material-ui/core/CircularProgress";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import Typography from "@material-ui/core/Typography";
 import FixtureDetailsCard from "./FixtureDetailsCard";
+import calculateColourFromBet from "../../functions/calculateColourFromBet";
 
-const FixtureList = ({ fixtures, bets }) => {
-  return (
-    <div>
-      {fixtures.map(fixture => {
-        let bet = bets.find(bet => bet.fixture === fixture.fixtureID);
-        let homeBet, awayBet, homeResult, awayResult;
-        homeResult = fixture.final_score.split(":")[0];
-        awayResult = fixture.final_score.split(":")[1];
-        if (bet) {
-          homeBet = bet.bet.split(":")[0];
-          awayBet = bet.bet.split(":")[1];
-        }
-        let resultColour = calculateColourFromBet(homeBet, awayBet, homeResult, awayResult); 
-        return (
-          <ExpansionPanel style={{
-              backgroundColor: resultColour,
-              backgroundImage: `linear-gradient(to bottom right, rgb(13,52,255), ${resultColour})`
-            }} 
-            key={fixture.fixtureID}>
-            <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-              <Typography style={{color: "white"}}>{`${fixture.home} - ${fixture.away}`}</Typography>
-            </ExpansionPanelSummary>
-            <ExpansionPanelDetails
-              style={{ backgroundColor: "rgb(250,250,250)" }}
+class FixtureList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      expanded: null
+    };
+  }
+
+  handleChange = panel => (event, expanded) => {
+    this.setState({
+      expanded: expanded ? panel : false
+    });
+  };
+
+  render() {
+    let { fixtures, bets } = this.props;
+    return (
+      <div>
+        {fixtures.map(fixture => {
+          let bet = bets.find(bet => bet.fixture === fixture.fixtureID);
+          let homeBet, awayBet, homeResult, awayResult;
+          homeResult = fixture.final_score.split(":")[0];
+          awayResult = fixture.final_score.split(":")[1];
+          if (bet) {
+            homeBet = bet.bet.split(":")[0];
+            awayBet = bet.bet.split(":")[1];
+          }
+          let resultColour = calculateColourFromBet(
+            homeBet,
+            awayBet,
+            homeResult,
+            awayResult
+          );
+          return (
+            <ExpansionPanel
+              expanded={this.state.expanded === fixture.fixtureID}
+              onChange={this.handleChange(fixture.fixtureID)}
+              key={fixture.fixtureID}
+              style={{
+                backgroundColor: resultColour,
+                backgroundImage: `linear-gradient(to bottom right, rgb(13,52,255), ${resultColour})`
+              }}
             >
-              <FixtureDetailsCard
-                date={fixture.date}
-                homeBet={homeBet}
-                awayBet={awayBet}
-                homeResult={homeResult}
-                awayResult={awayResult}
-              />
-            </ExpansionPanelDetails>
-          </ExpansionPanel>
-        );
-      })}
-    </div>
-  );
-};
+              <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+                <Typography style={{ color: "white" }}>{`${fixture.home} - ${
+                  fixture.away
+                }`}</Typography>
+              </ExpansionPanelSummary>
+              <ExpansionPanelDetails
+                style={{ backgroundColor: "rgb(250,250,250)" }}
+              >
+                <FixtureDetailsCard
+                  date={fixture.date}
+                  homeBet={homeBet}
+                  awayBet={awayBet}
+                  homeResult={homeResult}
+                  awayResult={awayResult}
+                />
+              </ExpansionPanelDetails>
+            </ExpansionPanel>
+          );
+        })}
+      </div>
+    );
+  }
+}
 
 export default FixtureList;
-
-const calculateColourFromBet = (betHome, betAway, resultHome, resultAway) => {
-    if(!betHome) return "red";
-    var betResult = setResult(betHome, betAway);
-    var fixtureResult = setResult(resultHome, resultAway);
-    if (betResult.result !== fixtureResult.result) return "red"; 
-    if (betHome === resultHome && betAway === resultAway) return "green";
-    else return "yellow";
-  };
-  
-  const setResult = (home, away) => {
-    if (home == away)
-      return {
-        result: "draw"
-      };
-    if (home > away)
-      return {
-        result: "home"
-      };
-    if (home < away)
-      return {
-        result: "away"
-      };
-  };
---
+++ src/functions/calculateColourFromBet.js
@@ -0,0 +1,25 @@
+const calculateColourFromBet = (betHome, betAway, resultHome, resultAway) => {
+  if (!betHome) return "red";
+  var betResult = setResult(betHome, betAway);
+  var fixtureResult = setResult(resultHome, resultAway);
+  if (betResult.result !== fixtureResult.result) return "red";
+  if (betHome === resultHome && betAway === resultAway) return "green";
+  else return "yellow";
+};
+
+const setResult = (home, away) => {
+  if (home == away)
+    return {
+      result: "draw"
+    };
+  if (home > away)
+    return {
+      result: "home"
+    };
+  if (home < away)
+    return {
+      result: "away"
+    };
+};
+
+export default calculateColourFromBet;
